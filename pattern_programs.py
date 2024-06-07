5ef0ae3f47b7076c514d0a497539f8436aa8126b
zakeer <zzakeer22@gmail.com>
2024-06-08 01:38:54 +0530

    82 patterns updated

--- pattern_programs.py
+++ pattern_programs.py
@@ -223,3 +223,305 @@ for i in range(1,n+1):
     print()
 
 print("\nPattern 43\n")
+for i in range(1,n+1):
+    print(" "*(n-i),end="")
+    for j in range(i-1,-1,-1):
+        print(j,end="")
+    for k in range(1,i):
+        print(k,end="")
+    print()
+
+print("\nPattern 44\n")
+for i in range(1,n+1):
+    print(" "*(n-i),end="")
+    for j in range(i-1,-1,-1):
+        print(chr(65+j),end="")
+    for k in range(1,i):
+        print(chr(65+k),end="")
+    print()
+        
+print("\nPattern 45\n")
+for i in range(1,n+1):
+    print(" "*(n-i),end="")
+    for j in range(1,i+1):
+        print(j,end="")
+    for k in range(i-1,0,-1):
+        print(k,end="")
+    print()
+
+print("\nPattern 46\n")
+for i in range(1,n+1):
+    print(" "*(n-i),end="")
+    for j in range(1,i+1):
+        print(chr(64+j),end="")
+    for k in range(1,i):
+        print(chr(64+k),end="")
+    print()
+
+print("\nPattern 47\n")   #red
+for i in range(1,n+1):
+    print(" "*(n-i),end="")
+    for j in range(n,n-i,-1):
+        print(j,end=" ")
+    print()
+
+print("\nPattern 48\n")
+for i in range(n,0,-1):
+    print(" "*(n-i),end="")
+    for j in range(i,0,-1):
+        print("*"*(2*j-1))
+        break
+
+print("\nPattern 49\n")
+for i in range(n,0,-1):
+    print(" "*(n-i)+str(i)*(2*i-1))
+
+print("\nPattern 50\n")
+for i in range(n,0,-1):
+    print(" "*(n-i)+str(2*i-1)*(2*i-1))
+
+print("\nPattern 51\n")
+for i in range(n,0,-1):
+    print(" "*(n-i),end="")
+    for j in range(1,2*i):
+        print(j,end="")
+    print()
+
+print("\nPattern 52\n")
+for i in range(n,0,-1):
+    print(" "*(n-i)+chr(64+i)*(2*i-1))
+
+print("\nPattern 53\n")
+for i in range(n,0,-1):
+    print(" "*(n-i)+chr(64+(2*i-1))*(2*i-1))
+
+print("\nPattern 54\n")
+for i in range(n,0,-1):
+    print(" "*(n-i),end="")
+    for j in range(1,2*i):
+        if j == 2*i//2 and j != 2*i-1:
+            print(chr(64+j)*2,end="")
+        elif j < 2*i-1 or j ==1:
+            print(chr(64+j),end="")
+    print()
+
+print("\nPattern 55\n") #red
+for i in range(1,n+1):
+    print(" "*(n-i),end="")
+    for j in range(1,i+1):
+        print(str(j),end=" ")
+    print()
+for i in range(n-1,0,-1):
+    print(" "*(n-i),end="")
+    for j in range(1,i+1):
+        print(str(j),end=" ")
+    print()
+
+print("\nPattern 56\n") #red
+for i in range(1,n+1):
+    print(" "*(n-i)+"* "*i)
+for i in range(n-1,0,-1):
+    print(" "*(n-i)+"* "*i)
+
+print("\nPattern 57\n")  #red
+for i in range(1,n+1):
+    print(" "*(n-i),end="")
+    for j in range(n-1,n-i-1,-1):
+        print(str(j),end=" ")
+    print()
+for i in range(n-1,0,-1):
+    print(" "*(n-i),end="")
+    for j in range(n-1,n-i-1,-1):
+        print(str(j),end=" ")
+    print()
+
+print("\nPattern 58\n")  #red
+for i in range(1,n+1):
+    print(" "*(n-i),end="")
+    for j in range(n-i,n):
+        print(str(j),end=" ")
+    print()
+for i in range(n-1,0,-1):
+    print(" "*(n-i),end="")
+    for j in range(n-i,n):
+        print(str(j),end=" ")
+    print()
+
+print("\nPattern 59\n")  #red
+for i in range(1,n+1):
+    print(" "*(n-i),end="")
+    for j in range(n-i,n):
+        print(chr(65+j),end=" ")
+    print()
+for i in range(n-1,0,-1):
+    print(" "*(n-i),end="")
+    for j in range(n-i,n):
+        print(chr(65+j),end=" ")
+    print()
+
+print("\nPattern 60\n")
+for i in range(1,n+1):
+    print("*"*i)
+for i in range(n-1,0,-1):
+    print("*"*i)
+
+print("\nPattern 61\n")
+for i in range(1,n+1):
+    for j in range(n-1,n-i-1,-1):
+        print(j,end="")
+    print()
+for i in range(n-1,0,-1):
+    for j in range(n-1,n-i-1,-1):
+        print(j,end="")
+    print()
+
+print("\nPattern 62\n")
+for i in range(1,n+1):
+    for j in range(n-i,n):
+        print(j,end="")
+    print()
+for i in range(n-1,0,-1):
+    for j in range(n-i,n):
+        print(j,end="")
+    print()
+
+print("\nPattern 63\n")
+for i in range(1,n+1):
+    print(chr(64+n-i+1)*i)
+for i in range(n-1,0,-1):
+    print(chr(64+n-i+1)*i)
+
+print("\nPattern 64\n")
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(chr(64+n-j+1),end="")
+    print()
+for i in range(n-1,0,-1):
+    for j in range(i):
+        print(chr(64+n-j+1),end="")
+    print()
+
+print("\nPattern 65\n")
+for i in range(1,n+1):
+    for j in range(i,0,-1):
+        print(chr(64+n-j+1),end="")
+    print()
+for i in range(n-1,0,-1):
+    for j in range(i,0,-1):
+        print(chr(64+n-j+1),end="")
+    print()
+
+print("\nPattern 66\n")
+for i in range(1,n+1):
+    print(" "*(n-i)+"* "*i)
+
+print("\nPattern 67\n")
+for i in range(1,n+1):
+    print(" "*(n-i)+(str(i)+" ")*i)
+
+print("\nPattern 68\n")
+for i in range(1,n+1):
+    print(" "*(n-i),end="")
+    for j in range(1,i+1):
+        print((str(j)),end=" ")
+    print()
+
+print("\nPattern 69\n")
+for i in range(1,n+1):
+    print(" "*(n-i)+(chr(64+i)+" ")*i)
+
+print("\nPattern 70\n")
+for i in range(1,n+1):
+    print(" "*(n-i),end="")
+    for j in range(1,i+1):
+        print(chr(64+j),end=" ")
+    print()
+
+print("\nPattern 71\n")
+for i in range(n,0,-1):
+    print(" "*(n-i)+"* "*i)
+
+print("\nPattern 72\n")
+for i in range(n,0,-1):
+    print(" "*(n-i)+(str(i)+" ")*i)
+
+print("\nPattern 73\n")
+for i in range(n,0,-1):
+    print(" "*(n-i),end="")
+    for j in range(i,0,-1):
+        print((str(j)),end=" ")
+    print()
+
+print("\nPattern 74\n")
+for i in range(n,0,-1):
+    print(" "*(n-i)+(chr(64+i)+" ")*i)
+
+print("\nPattern 75\n")
+for i in range(n,0,-1):
+    print(" "*(n-i),end="")
+    for j in range(i,0,-1):
+        print(chr(64+j),end=" ")
+    print()
+
+print("\nPattern 76\n")
+for i in range(n,0,-1):
+    print(" "*(n-i),end="")
+    for j in range(1,i+1):
+        print(chr(64+j),end=" ")
+    print()
+
+print("\nPattern 77\n")
+for i in range(1,n+1):
+    print(" "*(n-i)+"* "*i)
+for i in range(n-1,0,-1):
+    print(" "*(n-i)+"* "*i)
+
+print("\nPattern 78\n")
+for i in range(1,n+1):
+    print(" "*(n-i)+(str(i)+" ")*i)
+for i in range(n-1,0,-1):
+    print(" "*(n-i)+(str(i)+" ")*i)
+
+print("\nPattern 79\n")
+for i in range(1,n+1):
+    print(" "*(n-i),end="")
+    for j in range(1,i+1):
+        print((str(j)),end=" ")
+    print()
+for i in range(n-1,0,-1):
+    print(" "*(n-i),end="")
+    for j in range(n-i+1,n+1):
+        print((str(j)),end=" ")
+    print()
+
+print("\nPattern 80\n")
+for i in range(1,n+1):
+    print(" "*(n-i),end="")
+    for j in range(1,i+1):
+        print((str(j)),end=" ")
+    print()
+for i in range(n-1,0,-1):
+    print(" "*(n-i),end="")
+    for j in range(1,i+1):
+        print((str(j)),end=" ")
+    print()
+
+print("\nPattern 81\n")
+for i in range(1,n+1):
+    print(" "*(n-i)+(chr(64+i)+" ")*i)
+for i in range(n-1,0,-1):
+    print(" "*(n-i)+(chr(64+i)+" ")*i)
+
+print("\nPattern 82\n")
+for i in range(1,n+1):
+    print(" "*(n-i),end="")
+    for j in range(1,i+1):
+        print(chr(64+j),end=" ")
+    print()
+for i in range(n-1,0,-1):
+    print(" "*(n-i),end="")
+    for j in range(n-i+1,n+1):
+        print(chr(64+j),end=" ")
+    print()
+
+print("\nPattern 83\n")
