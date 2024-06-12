170c817a16da236f45b05c0bdc88f331d8d52a90
zakeer <zzakeer22@gmail.com>
2024-06-12 18:03:51 +0530

    changes committed

--- parenthesis.py
+++ parenthesis.py
@@ -1,8 +1,12 @@
+
+""" parenthesis using lists with
+                        more complexity
+"""
+
 o = ["[","{","("]
 c = ["]","}",")"]
 
 s = input("Enter the string: ")
-count = 0
 l = []
 for i in range(len(s)):
     if len(s)%2 == 0:
@@ -20,6 +24,27 @@ if len(l) == 0:
     print("True")
 else:
     print("False")
-    
-        
 
+
+""" parenthesis using dictionaries with
+                        less complexity 0(n)
+"""
+
+d ={"]" : "[", ")" : "(", "}":"{"}
+s = input("Enter the string: ")
+l = []
+for i in range(len(s)):
+    if len(s)%2 == 0:
+        if s[i] in list(d.values()):
+            l.append(s[i])
+        elif s[i] in list(d.keys()):
+            if d[s[i]] == l[-1]:
+                l.pop()
+        else:
+            break
+    else:
+        break
+if len(l) == 0:
+    print("True")
+else:
+    print("False")
