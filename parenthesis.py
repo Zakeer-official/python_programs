d674732f4028f78fbb90cce24038d989d9b4ec16
zakeer <zzakeer22@gmail.com>
2024-06-13 01:11:16 +0530

    changes committed

--- parenthesis.py
+++ parenthesis.py
@@ -8,22 +8,23 @@ c = ["]","}",")"]
 
 s = input("Enter the string: ")
 l = []
-for i in range(len(s)):
-    if len(s)%2 == 0:
-        if s[i] in o:
-            l.append(s[i])
+if len(s) != 0:
+    for i in range(len(s)):
+        if len(s)%2 == 0:
+            if s[i] in o:
+                l.append(s[i])
+            else:
+                for j in range(3):
+                    if s[i] == c[j]:
+                        m = j
+                if l[-1] == o[m]:
+                    l.pop()
         else:
-            for j in range(3):
-                if s[i] == c[j]:
-                    m = j
-            if l[-1] == o[m]:
-                l.pop()
+            break
+    if len(l) == 0:
+        print("True")
     else:
-        break
-if len(l) == 0:
-    print("True")
-else:
-    print("False")
+        print("False")
 
 
 """ parenthesis using dictionaries with
@@ -33,18 +34,19 @@ else:
 d ={"]" : "[", ")" : "(", "}":"{"}
 s = input("Enter the string: ")
 l = []
-for i in range(len(s)):
-    if len(s)%2 == 0:
-        if s[i] in list(d.values()):
-            l.append(s[i])
-        elif s[i] in list(d.keys()):
-            if d[s[i]] == l[-1]:
-                l.pop()
+if len(s) != 0:
+    for i in range(len(s)):
+        if len(s)%2 == 0:
+            if s[i] in list(d.values()):
+                l.append(s[i])
+            elif s[i] in list(d.keys()):
+                if d[s[i]] == l[-1]:
+                    l.pop()
+            else:
+                break
         else:
             break
+    if len(l) == 0:
+        print("True")
     else:
-        break
-if len(l) == 0:
-    print("True")
-else:
-    print("False")
+        print("False")
