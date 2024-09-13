42fc57e634d3416e4cfce87803a2908ee77e3022
Zakeer <zzakeer22@gmail.com>
2024-09-13 06:57:07 +0530

    linked list

--- Linked_list.py
+++ Linked_list.py
@@ -34,6 +34,73 @@ class LinkedList:
 
     def insert_after(self,after,value):
         new_node = Node(value)
+        curr = self.head
+        while curr != None:
+            if curr.data == after:
+                break
+            curr = curr.next
+        if curr != None:
+            new_node.next = curr.next
+            curr.next = new_node
+            self.n = self.n + 1
+        else:
+            return 'Item not found'
+    
+    def clear(self):
+        self.head = None
+        self.n = 0
+    
+    def delete_head(self):
+        if self.head == None :
+            return 'Empty'
+        self.head = self.head.next
+        self.n = self.n - 1
+
+    def delete_tail(self):
+        if self.head == None :
+            return 'Empty'
+        curr = self.head
+        if curr.next == None:
+            return self.delete_head()
+        while curr.next.next != None:
+            curr = curr.next
+        curr.next = None
+        self.n = self.n - 1
+
+    def delete_value(self,value):
+        if self.head == None :
+            return 'Empty'
+        if self.head.data == value :
+            return self.delete_head()
+        curr = self.head
+        while curr.next != None:
+            if curr.next.data == value:
+                break
+            curr = curr.next
+        if curr.next == None:
+            return 'Item not found'
+        else:
+            curr.next = curr.next.next
+
+    def search_value(self,item):
+        curr = self.head
+        pos = 0
+        while curr != None:
+            if curr.data == item:
+                return pos
+            curr = curr.next
+            pos = pos + 1
+        return "Item not found"
+
+    def __getitem__(self,index):
+        curr = self.head
+        pos = 0
+        while curr != None:
+            if pos == index:
+                return curr.data
+            curr = curr.next
+            pos = pos + 1
+        return 'IndexError'
 
 
     def traverse(self):
@@ -48,4 +115,5 @@ a = LinkedList()
 a.insert_head(1)
 a.insert_head(2)
 a.insert_tail(3)
+a.insert_after(3,4)
 print(a.traverse())
